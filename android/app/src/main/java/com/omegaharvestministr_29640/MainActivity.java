142448d9439d794bc248f049f3da7f6b12c0ac5d
Crowdbotics <team@crowdbotics.com>
2021-08-08 19:21:11 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/omegaharvestministr_29640/MainActivity.java
@@ -0,0 +1,15 @@
+package com.omegaharvestministr_29640;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "omegaharvestministr_29640";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "omegaharvestministr_29640Identifier",
+  url: "https://omegaharvestministr_29640Identifier.botics.co",
+  api: "https://omegaharvestministr_29640Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
